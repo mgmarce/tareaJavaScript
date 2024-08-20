b5366936e6b900e118212627c891435f3e3014ae
mgmarce <https.mgmarce@gmail.com>
2024-08-20 17:03:42 -0600

    Con README

---
+++ README.md
@@ -0,0 +1,11 @@
+# Actividad: 🌟
+
+_Guia estructuras de control JavaScript_
+
+## Datos estudiante: ✨ 
+
+_Marcela Menjivar_
+
+```
+fsj-24a
+```
--- info.js
+++
@@ -1,6 +0,0 @@
-console.log('********************************************************************')
-console.log('\tActividad: Guia estructuras de control JavaScript')
-console.log('\tUsuario: k00002511')
-console.log("\tEstudiante: Marcela Carolina Menjívar Gutiérrez")
-console.log('********************************************************************')
-
